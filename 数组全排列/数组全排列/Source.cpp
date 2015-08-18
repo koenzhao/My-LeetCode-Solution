1eb1091032600df93fccc1d0dcc770d15cddfc24
koenchiu <koen.chiu@gmail.com>
2015-08-18 18:31:34 +0800

    update

--- 数组全排列/数组全排列/Source.cpp
+++ 数组全排列/数组全排列/Source.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
-void perm(int list[], int k, int m)
+using namespace std;
+#define N 5
+void Print(int* arr, int length);
+void swap(int &a, int &b);
+
+void perm(int* arr, int* ptr, int length)
 {
-	if ()
+	if (length == 1)
 	{
-		copy(list, list + m, ostream_iterator<int>(cout, " "));
-		cout << endl;
+		Print(arr, N);
 		return;
 	}
-	for (int i = k; i <= m; i++)
+		
+
+	for (int i = 0; i < length; ++i)
 	{
-		swap(&list[k], &list[i]);
-		();
-		swap(&list[k], &list[i]);
+		swap(ptr[0], ptr[i]);
+		perm(arr, ptr+1, length-1);
+		swap(ptr[0], ptr[i]);
 	}
+}
+
+void Print(int* arr, int length)
+{
+	for (int i = 0; i < length; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+	cout << endl;
+}
+
+void swap(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+int main()
+{
+	int* arr = new int[];
+	for (int i = 0;i<N;++i)
+	{
+		arr[i] = i + 1;
+	}
+	perm(arr, arr, N);
+	
+	return 0;
 }
